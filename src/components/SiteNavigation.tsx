87bf4adb4dd15f698f35d0208eef3a5afb528ec1
julian pickard <jpickard.mail@gmail.com>
2026-01-24 14:10:55 +0000

    Added contact page back in

--- src/components/SiteNavigation.tsx
+++ src/components/SiteNavigation.tsx
@@ -45,6 +45,7 @@ export default function SiteNavigation() {
     { to: "/testimonials", label: "Reviews" },
     { to: "/faq", label: "FAQ" },
     { to: "/help", label: "Help" },
+    { to: "/contact", label: "Contact" },
     { to: "/about", label: "About" },
   ];
 
